110440e6f1e064517baa970913da50232ef37b89
vphatfla <pvphat25@gmail.com>
2021-07-15 15:15:20 -0400

    finish functions, neeed: design + keydown + test

---
+++ script.js
@@ -0,0 +1,101 @@
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const decimalButton = document.querySelector('.decimal');
+const equalButton = document.querySelector('.equal');
+const screen = document.getElementById('display');
+let result = 0;
+let operator = '+';
+let calculatedCheck = false;
+
+numberButtons.forEach( (numberButton) => 
+numberButton.addEventListener('click', () => displayNumber(numberButton.textContent))
+);
+decimalButton.addEventListener('click', () =>displayNumber(decimalButton.textContent));
+operationButtons.forEach( (operationButton) =>
+operationButton.addEventListener('click', () => operation(operationButton.textContent))
+);
+
+clearButton.addEventListener('click', () => resetScreen());
+deleteButton.addEventListener('click', () => deleteScreen());
+equalButton.addEventListener('click', () => equalButtonDisplay());
+
+
+function deleteScreen(){
+    screen.textContent = screen.textContent.substring(0, screen.textContent.length-1);
+}
+function resetScreen(){
+    operator = '+';
+    result = 0;
+    displayResult(result);
+    console.log(result);
+}
+
+
+function displayNumber(content){
+    if ((screen.textContent == 0) || ((result != 0) && (round(result) == round(screen.textContent) )) ) screen.textContent = content;
+    else screen.textContent += content;
+    calculatedCheck  = false;
+}
+function round(number){
+    return Math.round(number*1000)/1000;
+}
+function displayResult(result){
+    result = round(result);
+    screen.textContent = result;
+}
+
+function equalButtonDisplay(){
+    if (!calculatedCheck) {
+        let currentNumber = Number(screen.textContent);
+        result = calculate(result, currentNumber, operator);
+        calculatedCheck = true;
+    }
+    displayResult(result);
+}
+function operation(operatorSign){
+    if (!calculatedCheck) {
+        let currentNumber = Number(screen.textContent);
+        result = calculate(result, currentNumber, operator);
+        displayResult(result);
+        calculatedCheck = true;
+    }
+    operator = operatorSign;
+}
+
+//call math functions
+function calculate(a,b, operator){
+   switch (operator){
+    case '+' :
+       return plus(a,b);
+       break;
+    case '-':
+        return minus(a,b);
+        break;
+    case '/':
+        return devide(a,b);
+        break;
+    case 'x':
+        return multiply(a,b);
+    }
+}
+// math functions
+function plus(a,b){
+    return a+b;
+}
+
+function minus(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function devide(a,b){
+    if (b==0) {
+        alert('Never devided by 0');
+        resetScreen();
+        return 0;
+    }
+    return a/b;
+}
